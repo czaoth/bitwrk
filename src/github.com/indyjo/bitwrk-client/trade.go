cef82119f92d598b99feea731d06ba3376e44eac
Jonas Eschenburg <jonas@bitwrk.net>
2017-06-25 17:23:48 +0200

    Fix compile error

--- src/github.com/indyjo/bitwrk-client/trade.go
+++ src/github.com/indyjo/bitwrk-client/trade.go
@@ -421,7 +421,7 @@ func (t *Trade) GetState() *ActivityState {
 	}
 
 	if t.workFile != nil {
-		result.BytesTotal = uint64(t.workFile.Size())
+		result.BytesTotal = t.workFile.Size()
 	}
 
 	return result
